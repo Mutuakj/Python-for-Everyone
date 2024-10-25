dc82be3a60155c06471e4412e08519de4e27f9fa
mutuakj <106160177+Mutuakj@users.noreply.github.com>
2024-10-25 03:04:26 +0300

    Add files via upload
    Summary
    Use the for loop statement to run a code block a fixed number of times.
    Use the range(start, stop, step) to customize the loop.

---
+++ for_loop.py
@@ -0,0 +1,14 @@
+# Using Python for loop to calculate the sum of a sequence
+# The following example uses for to calculate the sum of numbers from 1 to 100
+
+sum = 0
+for num in range(101):
+    sum += num
+
+print(sum)
+
+# How it works
+"""First, the sum is initialized to zero
+Second, the sum is added with the number from 1 to 100 in each iteration
+Finally, show the sum to the screen"""
+
